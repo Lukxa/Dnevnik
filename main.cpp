0dcc8c995197c7d947f5c7eb3ce1fc8081668d10
Lukxa <104598825+Lukxa@users.noreply.github.com>
2023-05-24 16:19:44 +0200

    Added main file

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Datum{
+    int dd,mm,gg;
+};
+
+struct Osoba{
+    string ime,prezime;
+    int razred;
+    char odjeljenje;
+    Datum d;
+};
+
+int main()
+{
+    struct Osoba uc[50];
+    int br;
+    cout<<"Unesi broj ucenika (max.50): ";
+    cin>>br;
+    
+    if(br>50){
+        cout<<"Maksimalan broj ucenika je 50!";
+        return 0;
+    }
+    
+    for(int i=0;i<br;i++){
+        cout<<"Ucenik"<<i+1<<endl;
+        cout<<"Ime: ";
+        cin>>uc[i].ime;
+        cout<<"Prezime: ";
+        cin>>uc[i].prezime;
+        cout<<"Razred: ";
+        cin>>uc[i].razred;
+        cout<<"Odjeljenje: ";
+        cin>>uc[i].odjeljenje;
+        cout<<"Dan rodjenja: ";
+        cin>>uc[i].d.dd;
+        cout<<"Mjesec rodjenja: ";
+        cin>>uc[i].d.mm;
+        cout<<"Godina rodjenja: ";
+        cin>>uc[i].d.gg;
+        
+    }
+
+    return 0;
+}
